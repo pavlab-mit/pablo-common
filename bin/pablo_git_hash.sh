698713c1c7e14ec2961bfa7c5cd3edf01b7377a3
mikerb <mikerb@mit.edu>
2026-05-06 20:33:03 -0400

    mods by mikerb Wed May  6 20:33:03 EDT 2026

--- bin/pablo_git_hash.sh
+++ bin/pablo_git_hash.sh
@@ -8,8 +8,7 @@
 # Part 1: Initialize script variables
 #--------------------------------------------------------
 ME=`basename "$0"`
-CHECK_DIR=""
-TREE=""
+REPO=""
 TERSE=""
 
 #--------------------------------------------------------
@@ -20,30 +19,19 @@ for ARGI; do
         echo "$ME [OPTIONS]                                     "
         echo "                                                  " 
         echo "Synopsis:                                         " 
-        echo "  The $ME script will return the SVN revision     "
-        echo "  number of the specified tree.                   "
+        echo "  The $ME script will return the git hash of the  "
+        echo "  specified repo                                  "
         echo "                                                  " 
-        echo "Options  :                                        " 
+        echo "Options:                                          " 
         echo "  --help, -h                                      "
 	echo "  --terse, -t           No newline char in output "
         echo "                                                  " 
-        echo "  --pablo-common, -pac  Check pablo-common tree   "
-        echo "  --moos, -m            Check moos-ivp tree       "
-        echo "  --swarm, -s           Check moos-ivp-swarm tree "
-        echo "  --2680, -2            Check moos-ivp-2680       "
+        echo "  --repo=<repo>         Name of repo to query     " 
         exit 0;
+    elif [ "${ARGI:0:7}" = "--repo=" ]; then
+        REPO="${ARGI#--repo=*}"
     elif [ "${ARGI}" = "--terse" -o "${ARGI}" = "-t" ]; then
         TERSE="-n"
-    elif [ "${ARGI}" = "--moos" -o "${ARGI}" = "-m" ]; then
-        TREE="moos-ivp"
-    elif [ "${ARGI}" = "--pablo-common" -o "${ARGI}" = "-pac" ]; then
-        TREE="pablo-common"
-    elif [ "${ARGI}" = "--moos-ivp-swarm" -o "${ARGI}" = "-s" ]; then
-        TREE="moos-ivp-swarm"
-    elif [ "${ARGI}" = "--moos-ivp-pavlab" -o "${ARGI}" = "-pav" ]; then
-        TREE="moos-ivp-pavlab"
-    elif [ "${ARGI}" = "--2680" -o "${ARGI}" = "-2" ]; then
-        TREE="moos-ivp-2680"
     else
 	echo "$ME: Bad Arg: $ARGI. Exit Code 1."
         exit 1
@@ -53,37 +41,25 @@ done
 #--------------------------------------------------------
 # Part 3: Verify that a tree has been specified
 #--------------------------------------------------------
-if [ "${TREE}" = "" ]; then
-    echo "A tree must be specified, -m, -s, -p or -2. Exit Code 2."
+if [ "${REPO}" = "" ]; then
+    echo "Repo must be specified with --repo arg. Exit 2."
     exit 2;
 fi
 
 
 #--------------------------------------------------------
-# Part 4: Verify existence and location of tree to update
+# Part 4: Verify existence and location of repo.
 #--------------------------------------------------------
-TREE_ARO=${TREE}"-aro"
-
-cd 
-if [ -d ./$TREE ]; then
-    CHECK_DIR="./$TREE"
-elif [ -d ./$TREE_ARO ]; then
-    CHECK_DIR="./$TREE_ARO"
-elif [ -d ./Research/$TREE ]; then
-    CHECK_DIR="./Research/$TREE"
-elif [ -d ./Research/$TREE_ARO ]; then
-    CHECK_DIR="./Research/$TREE_ARO"
-fi
-
-if [ "${CHECK_DIR}" = "" ]; then
-    echo "Could not find $TREE or $TREE_ARO. Exit Code 3."
+FULL_REPO="$HOME/$REPO"
+if [ ! -d "${FULL_REPO}" ]; then
+    echo "Could not find [${REPO}]. Exit 3."
     exit 3;
 fi
 
 #--------------------------------------------------------
-# Part 5: Execute SVN info and grep for revision number
+# Part 5: Execute the git command
 #--------------------------------------------------------
-cd $CHECK_DIR
+cd $FULL_REPO
 
 a=`git rev-parse --short HEAD`
 
